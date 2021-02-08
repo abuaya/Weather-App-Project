48a38659ce1d0df125a0984733d3518a2eb7d9d7
abuaya <74809348+abuaya@users.noreply.github.com>
2021-02-08 11:07:25 +1100

    changed a bit of styling and html. Major additions of weather elements in JS

--- src/index.js
+++ src/index.js
@@ -39,3 +39,26 @@ let months = [
 let month = months[now.getMonth()];
 
 h1.innerHTML = `${day} ${month} ${date}, ${year}, ${hours}:${minutes}`;
+
+///////
+function displayTemperature (response) {
+
+  console.log(response.data)
+
+  let temperatureElement = document.querySelector("#temperatureData");
+  temperatureElement.innerHTML = Math.round (response.data.main.temp);
+  let cityElement = document.querySelector("#displayCity");
+  cityElement.innerHTML = response.data.name;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round (response.data.wind.speed);
+}
+ 
+
+  let apiKey = "a05b99834a4c4393485b5df92793ef0c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature)
